b4198de8f9f942d938c62c177171bfa2469273bb
Johannes Boyne <johannes@boyne.de>
2014-02-27 17:04:36 +0100

    improving

--- index.js
+++ index.js
@@ -43,15 +43,30 @@ module.exports.get = function (url, fn) {
 };
 module.exports.createWritePipe = function (_url, port, fn) {
   var parsedStorage = url.parse(_url);
-  var s = through();
   if (!fn) {
     fn = port;
     port = 8124;
   }
+  var s = through();
   var client = net.connect({port: port, host: parsedStorage.hostname},
                            function() { //'connect' listener
                              client.write(JSON.stringify({zen: { pipeData: true, id: parsedStorage.path.replace(/\//, '') }}));
                              s.pipe(client);
+                             fn(s);
+                           });
+};
+module.exports.follow = function (followName, _url, port, fn) {
+  var parsedStorage = url.parse(_url);
+  if (!fn) {
+    fn = port;
+    port = 8124;
+  }
+  var s = through();
+  var client = net.connect({port: port, host: parsedStorage.hostname},
+                           function() { //'connect' listener
+                             client.write(JSON.stringify({zen: { update: true, name: followName }}));
+                             client.write('\n');
+                             client.pipe(s);
                              fn(s);
                            });
 };
